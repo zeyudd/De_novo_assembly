92c39b5e12ed97782942daa2ae83c84dd9f38def
dding <dding@net19pc167.cse.psu.edu>
2018-04-09 09:37:46 -0400

    ...

--- upc_kmer_hash.h
+++ upc_kmer_hash.h
@@ -60,8 +60,8 @@ int add_kmer(shared bucket_t *hashtable, int64_t hashtable_size, shared kmer_t *
    int64_t pos = *posInHeap;
       
    /* Add the contents to the appropriate kmer struct in the heap */
-   upc_memput((kmers[pos * KMER_PACKED_SIZE], packedKmer, KMER_PACKED_LENGTH * sizeof(char));
-   heap[pos].kmer = &kmers[pos * KMER_PACKED_SIZE];
+   upc_memput(kmers[pos * KMER_PACKED_LENGTH], packedKmer, KMER_PACKED_LENGTH * sizeof(char));
+   heap[pos].kmer = &kmers[pos * KMER_PACKED_LENGTH];
    heap[pos].l_ext = left_ext;
    heap[pos].r_ext = right_ext;
    
