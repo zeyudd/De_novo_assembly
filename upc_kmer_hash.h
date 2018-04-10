6066694bb3552c207ddddc9acc7d50eba6cbd851
dding <dding@net19pc179.cse.psu.edu>
2018-04-10 05:48:57 -0400

    Fix bugs

--- upc_kmer_hash.h
+++ upc_kmer_hash.h
@@ -18,7 +18,8 @@ int64_t hashseq(int64_t  hashtable_size, char *seq, int size)
       hashval = seq[i] +  (hashval << 5) + hashval;
    }
    
-   return hashval % hashtable_size;
+   int64_t ret = hashval % hashtable_size;
+   return (ret < 0)? ret +  hashtable_size, ret;
 }
 
 /* Returns the hash value of a kmer */
