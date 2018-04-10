4ed9f57db06bdf16406b88de0d6fbed1775b3526
dding <dding@net19pc179.cse.psu.edu>
2018-04-10 06:31:40 -0400

    Fix bugs

--- init.sh
+++
@@ -1,7 +0,0 @@
-#export PATH=$PATH:/pylon5/cc3uv3p/bbrock/public/bin:/pylon5/cc3uv3p/bbrock/public/opt/bin
-#export MANPATH=$MANPATH:/pylon5/cc3uv3p/bbrock/public/share/man
-
-module use /gpfs/group/cfp102/cse597/sw/modules/
-module load upc
-
-qsub -I -A open -l nodes=1:ppn=4:scivybridge -l walltime=24:0:0 upcrun -np 3 -shared-heap=1G ./pgen test
